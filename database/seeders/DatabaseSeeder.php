a135ca644bb9d1599fcbade82d5cb5297cb4d3c6
abosala7 <mohamed_Felfl@yahoo.com>
2022-11-20 00:02:43 +0200

    offer factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Meal;
+use App\Models\Offer;
 use App\Models\Order;
 use Illuminate\Database\Seeder;
 
@@ -17,7 +18,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          Order::factory(10)->create();
-         Meal::factory(80)->create();
+         Meal::factory(20)->create();
+         Offer::factory(20)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
